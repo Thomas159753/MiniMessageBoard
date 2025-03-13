85d45c03aa5d26abb7ff00b850bc3cdc2c77486e
Thomas159753 <99398525+Thomas159753@users.noreply.github.com>
2025-03-13 20:08:00 +1100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.set("views", path.join(__dirname, "Views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
